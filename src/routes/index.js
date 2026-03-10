29c1064816324a6969f416e828bb5ab1859e3fd7
paras <dimplesaini5252@gmail.com>
2026-03-10 14:36:03 +0530

    Master Module: Added the customers API in  master module

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import { Router } from "express";
 const globalRouter = Router();
 
 import productRouter from "../modules/master/products/product.routes.js";
+import customerRouter from "../modules/master/customers/customer.routes.js";
 
 globalRouter.use("/products", productRouter);
+globalRouter.use("/customers", customerRouter);
 
 export default globalRouter;
 
-
+ 
 
